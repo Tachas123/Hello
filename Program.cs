c137fda9bb69ab926783645d680bf3f6e7d5491e
Tachas123 <Maciej.Tachasiuk@gmail.com>
2020-02-20 18:28:11 +0100

    Zależne odnośnie wieku

--- Program.cs
+++ Program.cs
@@ -7,14 +7,74 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string name = args[0];
+            ValueTypes();
+            for (; ; )
+            {
+                Greeting();
+                Age();
+                Settings();
+            }
+        }
+
+        private static void ValueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long MinLong = long.MinValue;
+
+            Console.WriteLine("maxInt = " + maxInt);
+            Console.WriteLine("minInt = " + minInt);
+            Console.WriteLine("maxLong = " + maxLong);
+            Console.WriteLine("MinLong = " + MinLong);
+        }
+
+        /// <summary>
+        /// Ustawienia powrotne domyślne
+        /// </summary>
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Wypisujemy komunikat zależny od wieku
+        /// </summary>
+        private static void Age()
+        {
+            Console.Write("Wpisz ile masz lat: ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Brawo jesteś pełnoletni, możesz wypić browara!");
+            }
+
+            else if (result == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Wprowadziłeś niepoprawny wiek!");
+            }
+
+            else
+            {
+                Console.WriteLine("Możemy Ci zaoferować mleko");
+            }
+        }
+
+        /// <summary>
+        /// Wypisujemy powitanie użytkownika
+        /// </summary>
+        private static void Greeting()
+        {
+            Console.Write("Wpisz swoje imię: ");
+            string name = Console.ReadLine();
 
             Console.WriteLine("Witaj, " + name);
-
-            //foreach (var item in args)
-            //{
-            //    Console.WriteLine("Witaj, " + item);
-            //}
         }
     }
 }
